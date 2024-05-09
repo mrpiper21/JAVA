3eb0ad7ef4965c4aa127df33f100a0918665daf2
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2024-05-09 04:39:32 +0000

    String Literals

---
+++ Strings/String_1.java
@@ -0,0 +1,18 @@
+package Strings;
+ // There are two ways to create a string
+  // 1. String literal -> To make Java more memory efficient (because no new
+  // objects are created if it exists already in the string constant pool
+ // 2. Using new Keyword -> In such a case, JVM will create a new string object in normal
+ // (non-pool) heap memory and the literal “Welcome” will be placed in the string constant pool. The variable s will refer to the object in the heap (non-pool)
+
+public class String_1 {
+    public static void main(String args[]){
+        String StringLiteral = "hello Letral";
+
+        String NewKeyString = new String("Hello new String key");
+        NewKeyString = "hola";
+        StringLiteral = "Jimmy";
+        System.out.println(StringLiteral + " " + NewKeyString);
+
+    };
+}
