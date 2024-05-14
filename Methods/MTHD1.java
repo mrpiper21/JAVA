48761dab0c1df7a8419db72aab2bcdf75f550425
Bernard <benbaah104@gmail.com>
2024-05-14 01:17:06 +0000

    update

---
+++ Methods/MTHD1.java
@@ -0,0 +1,39 @@
+package Methods;
+
+public class MTHD1 {
+    public static class Example {
+        private int number;
+        private String name;
+    
+        public int getNumber(){
+            return number;
+        };
+    
+        public String getName(){
+            return name;
+        }
+    
+        public void setName(String name){
+            this.name = name;
+        };
+    
+        public void setNumber(int number){
+            this.number = number;
+        }
+    
+        public void printDetails(){
+            System.out.printf("Number: %d\n", number);
+            System.out.printf("Name: %s", name);
+        }
+    }
+    public static void main(String args[]){
+        Example example = new Example();
+
+        example.setName("Bernard");
+        example.setNumber(33);
+        example.printDetails();
+    }
+}
+
+
+
