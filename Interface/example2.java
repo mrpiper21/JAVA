61ee087d0dff980c59887147a5c7cb091d1f8e25
Bernard <benbaah104@gmail.com>
2024-05-14 02:19:41 +0000

    interface implementation

--- Interface/example2.java
+++ Interface/example2.java
@@ -27,13 +27,40 @@ class Bicycle implements Verhicle {
     }
 }
 
+class MotorBike implements Verhicle{
+    int gear;
+    int speed;
+
+    @Override public void speedUp(int increment){
+        speed = speed + increment;
+    }
+
+    @Override public void changGear(int newGear){
+        gear = newGear;
+    }
+
+    @Override public void applyBrakes(int decrement){
+        speed -= decrement;
+    }
+
+    public void printState(){
+        System.out.println("Speed: " + speed + " gear: " + gear);
+    }
+}
+
 class example2 {
     public static void main(String args[]){
         Bicycle bicycle = new Bicycle();
         bicycle.changGear(2);
         bicycle.speedUp(3);
         bicycle.applyBrakes(1);
-        System.out.println("bicycle State: ");
+        System.out.print("bicycle State: ");
         bicycle.printStates();
+
+        MotorBike motor = new MotorBike();
+        motor.changGear(4);
+        motor.speedUp(6);
+        System.out.print("Motor state: ");
+        motor.printState();
     }
 }
