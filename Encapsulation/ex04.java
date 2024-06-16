529707e0b71cb12ad8909ec3bd87d7c619c0447e
mrpiper21 <benbaah104@gmail.com>
2024-06-16 18:18:51 +0000

    update

--- Encapsulation/ex04.java
+++ Encapsulation/ex04.java
@@ -1,44 +1,69 @@
 package Encapsulation;
-import Encapsulation.Area;
+
 class Account {
     private long acc_no;
     private String name, email;
     private float amount;
 
-    public long getAcc_no(){ return acc_no; };
-    public void setAcc_no(long ACC_NO){ this.acc_no = ACC_NO; };
+    public long getAcc_no() {
+        return acc_no;
+    };
 
-    public String getName(){ return name; };
-    public void setName(String NAME){ this.name = NAME;};
+    public void setAcc_no(long ACC_NO) {
+        this.acc_no = ACC_NO;
+    };
 
-    public String getEmail(){ return email; };
-    public void setEmail(String EMAIL){ this.email = EMAIL;};
+    public String getName() {
+        return name;
+    };
 
-    public float getAmount() { return amount; };
-    public void setAmount(float AMOUNT) { this.amount = AMOUNT; };
+    public void setName(String NAME) {
+        this.name = NAME;
+    };
 
-    public String[] getUserInfo(){
+    public String getEmail() {
+        return email;
+    };
+
+    public void setEmail(String EMAIL) {
+        this.email = EMAIL;
+    };
+
+    public float getAmount() {
+        return amount;
+    };
+
+    public void setAmount(float AMOUNT) {
+        this.amount = AMOUNT;
+    };
+
+    @Override
+    public String toString() {
+        return "Account [acc_no= " + acc_no + ", name= " + name + ", email= " + email + ", amount= " + amount + "]";
+    }
+
+    /**
+     * @return
+     */
+    public String[] getUserInfo() {
         // String acc = String.format( "%s", acc_no);
         String acc = "" + acc_no;
         // String amnt = String.format( "%s", amount);
         String amnt = "" + acc_no;
-        String[] info = {name, email, acc, amnt};
+        String[] info = { name, email, acc, amnt };
         return info;
     }
 
-}
-
-public class ex04 {
-    public static void main(String args[]){
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
         Account John = new Account();
         John.setAcc_no(480939845859493L);
         John.setAmount(4000);
         John.setName("john Doe");
         John.setEmail("JohnDoe@email.com");
-
-        String[] JonDoe = John.getUserInfo();
-        for(int i = 0; i < JonDoe.length; i++){
-            System.out.println(JonDoe[i]);
-        }
+        String JonDoe = John.toString();
+        System.out.println(JonDoe);
     }
 }
